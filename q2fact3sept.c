b304fccf909f2c4be81ef879590915b8e03f5b18
ananyapant663 <ananyapant663@gmail.com>
2025-09-03 17:00:07 +0530

    Add files via upload

---
+++ q2fact3sept.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int num,i=1,fact=1;
+    printf("enter a number: ");
+    scanf("%d",&num);
+    while(i<=num)
+    {
+        fact=fact*i;
+        i=i+1;
+    }
+    printf("Factorial of %d is %d",num,fact);
+    return 0;
+}
