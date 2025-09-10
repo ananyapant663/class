8e9c0413ccc935be1473f38d06ebd0eca601b958
ananyapant663 <ananyapant663@gmail.com>
2025-09-10 13:38:52 +0530

    Add files via upload

---
+++ descen.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main()
+{
+    int num, i,search,temp,j; 
+    printf ("Enter the number of values: ");
+    scanf("%d", &num);
+
+    int a[num];
+    for(i=0;i<num;i++)
+    {
+        scanf("%d", &a[i]);
+    }
+     for(i=0;i<5;i++) 
+     {
+        for( j =i+1;j<5;j++) 
+        {
+            if(a[i]>a[j])
+            {
+                temp = a[i];
+                a[i]= a[j];
+                a[j] =temp;
+            }
+        }
+    }
+    printf("Numbers in des order: ");
+    for(i=4; i>=0; i--) {
+        printf("%d ", a[i]);
+    }
+    return 0;   
+}
+
